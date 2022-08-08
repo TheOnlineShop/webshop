f560c9aae9024b967a77f49c6c2272d75d53b5bd
jonasbender <jonas.bender@tum.de>
2022-08-08 04:01:51 +0200

    Updates

---
+++ precache-manifest.cc726638f879845337325b3e77f0f13e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8181e0df4d3e8292f6a05731d429c0",
+    "url": "/webshop/index.html"
+  },
+  {
+    "revision": "99851207e96cc2fafb20",
+    "url": "/webshop/static/css/main.9848d6fa.chunk.css"
+  },
+  {
+    "revision": "69119e623f18f4984550",
+    "url": "/webshop/static/js/2.e8afb50f.chunk.js"
+  },
+  {
+    "revision": "fbf7a8d3bbea906e2d23f11649844255",
+    "url": "/webshop/static/js/2.e8afb50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99851207e96cc2fafb20",
+    "url": "/webshop/static/js/main.09dd8217.chunk.js"
+  },
+  {
+    "revision": "1bb454b0e2ac8290d4c8",
+    "url": "/webshop/static/js/runtime-main.7360cb70.js"
+  },
+  {
+    "revision": "f9637bc99911eefa28ebcf308457b3a1",
+    "url": "/webshop/static/media/Logo.f9637bc9.jpg"
+  },
+  {
+    "revision": "28a304acd288123d6da098094bde1811",
+    "url": "/webshop/static/media/icon.28a304ac.svg"
+  }
+]);
